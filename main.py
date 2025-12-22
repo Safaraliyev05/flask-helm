aa20959bfa2ba5794413ca2dc56328b668eb6cff
Sardor <safaraliyevsardor05@gmail.com>
2025-12-22 22:54:11 +0500

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,10 +10,14 @@ def hello():
     return "Hello from Dockerized Flask app! 🚀"
 
 
-@app.route("/health")
+@app.route("/health", methods=["GET"], strict_slashes=False)
 def health():
-    return jsonify({"status": "ok"}), 200
+    return jsonify(status="ok")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(
+        host=os.environ.get("FLASK_RUN_HOST", "0.0.0.0"),
+        port=int(os.environ.get("FLASK_RUN_PORT", 8080)),
+        debug=False
+    )
